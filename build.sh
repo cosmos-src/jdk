2d78b70853ca2f00dc812704f6627ab85f6aa5c5
pengma <764752624@qq.com>
2023-05-16 12:29:04 +0800

    macosx64 jdk17编译

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash ./configure --enable-warnings-as-errors=no --with-native-debug-symbols=internal --with-debug-level=slowdebug --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.0.7+7/Contents/Home
